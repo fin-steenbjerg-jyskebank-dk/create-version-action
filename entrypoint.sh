ac712db117c0c9bed0b4c0959aa1535c33718bd4
Fin Steenbjerg <fin.steenbjerg@gmail.com>
2021-10-24 12:38:15 +0200

    bug fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,6 @@ version=$(echo $GITHUB_REF | cut -d / -f 3)
 echo "imprinting version $version, git sha $GITHUB_SHA and time $time"
 sed -i "s/##VERSION##/$version/g" "$1"
 sed -i "s/##BUILD_TIME##/$time/g" "$1"
-sed -i "s/##BUILD_TIME##/$GITHUB_SHA/g" "$1"
+sed -i "s/##SHA##/$GITHUB_SHA/g" "$1"
 
 echo "::set-output name=time::$time"
